1969c49b0092e6ec82f290daedfb13259daa59bc
root <root@polka.(none)>
2013-02-28 12:53:18 +0000

    Trying to run 4 motors

--- Stepper.py
+++ Stepper.py
@@ -8,18 +8,20 @@ import RPi.GPIO as GPIO
  
 class Motor:
     def __init__(self, pins, revs_per_minute):
-        self.P1 = pins[0]
-        self.P2 = pins[1]
-        self.P3 = pins[2]
-        self.P4 = pins[3]
-        self.deg_per_step = 5.625 / 64
-        self.steps_per_rev = int(360 / self.deg_per_step)  # 4096
-        self.step_angle = 0  # Assume the way it is pointing is zero degrees
         for p in pins:
             GPIO.setup(p, GPIO.OUT)
             GPIO.output(p, 0)
 
+        self.P1 = pins[0]
+        self.P2 = pins[1]
+        self.P3 = pins[2]
+        self.P4 = pins[3]
+
+        self.deg_per_step = 5.625 / 64
+        self.steps_per_rev = int(360 / self.deg_per_step)  # 4096
+        self.step_angle = 0  # Assume the way it is pointing is zero degrees
         self._rpm = revs_per_minute
+
         # T is the amount of time to stop between signals
         self._T = (60.0 / self._rpm) / self.steps_per_rev
  
@@ -43,29 +45,31 @@ class Motor:
         GPIO.output(self.P2, 0)
         GPIO.output(self.P3, 0)
         GPIO.output(self.P4, 0)
-        for i in range(big_steps):
-            GPIO.output(self.P1, 0)
-            sleep(self._T)
-            GPIO.output(self.P3, 1)
-            sleep(self._T)
-            GPIO.output(self.P4, 0)
-            sleep(self._T)
-            GPIO.output(self.P2, 1)
-            sleep(self._T)
-            GPIO.output(self.P3, 0)
-            sleep(self._T)
-            GPIO.output(self.P1, 1)
-            sleep(self._T)
-            GPIO.output(self.P2, 0)
-            sleep(self._T)
-            GPIO.output(self.P4, 1)
-            sleep(self._T)
+
+	for i in range(big_steps):
+		GPIO.output(self.P4, 1)
+		sleep(self._T)
+		GPIO.output(self.P2, 0)
+		sleep(self._T)
+		GPIO.output(self.P3, 1)
+		sleep(self._T)
+		GPIO.output(self.P1, 0)
+		sleep(self._T)
+		GPIO.output(self.P2, 1)
+		sleep(self._T)
+		GPIO.output(self.P4, 0)
+		sleep(self._T)
+		GPIO.output(self.P1, 1)
+		sleep(self._T)
+		GPIO.output(self.P3, 0)
+		sleep(self._T)
  
     def _move_cw(self, big_steps):
         GPIO.output(self.P1, 0)
         GPIO.output(self.P2, 0)
         GPIO.output(self.P3, 0)
         GPIO.output(self.P4, 0)
+
         for i in range(big_steps):
             GPIO.output(self.P3, 0)
             sleep(self._T)
--- test-run.py
+++ test-run.py
@@ -9,25 +9,11 @@ if __name__ == "__main__":
 	m = Motor([18,22,24,26], 10)
 	#print "Pause in seconds: " + `m._T`
 
-	# anti-clock 90
-	print "Anti clock 90 degrees"
-	m.move_to(90)
-	sleep(1)
-
-	# clockw 90
-	print "Clockwise 90 degrees"
-	m.move_to(0)
-	sleep(1)
-
-	# anti-clock 180
-	print "Anti-clock 180"
-	m.move_to(180)
-	sleep(1)
-
 	# clock 180
 	print "Clock 180"
 	m.move_to(90)
+	sleep(0.5)
 	m.move_to(0)
-	sleep(1)
+	sleep(0.5)
 	GPIO.cleanup()
 
