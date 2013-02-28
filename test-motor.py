733379bf887bed9d5a166271366f707994266d52
root <root@polka.(none)>
2013-02-28 14:11:35 +0000

    Added scripts to test all 4 motors at once

--- test-motor.py
+++ test-motor.py
@@ -7,16 +7,14 @@ import sys
 
 if __name__ == "__main__":
 	GPIO.setmode(GPIO.BOARD)
-	#m = Motor([18,22,24,26], 10)
-	#m = Motor([sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4]], 10)
-	m = Motor([int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]),int(sys.argv[4])], 10)
-	#print "Pause in seconds: " + `m._T`
+	m = Motor([int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]),int(sys.argv[4])], 15)
 
 	# clock 180
-	print "Clock 180"
-	m.move_to(90)
+	m.move_to(180)
+	sleep(0.2)
+	m.move_to(270)
 	sleep(0.5)
 	m.move_to(0)
-	sleep(0.5)
+	m.move_to(-90)
 	GPIO.cleanup()
 
